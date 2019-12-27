97a39c69479b30deb0f803c4508980793c2217fd
theriley106 <christopherlambert106@gmail.com>
2019-12-27 00:34:06 -0500

    Added the create song function

--- main.py
+++ main.py
@@ -5,8 +5,22 @@ import glob
 print(cv2.__version__)
 from PIL import Image, ImageStat
 from mutagen.mp4 import MP4
+import pytesseract
+from PIL import Image, ImageDraw, ImageFont
+ 
 
 
+def recreate_image(fileName):
+	text = pytesseract.image_to_string(Image.open(fileName))
+	x = Image.open(fileName)
+	width, height = x.size
+	img = Image.new('RGB', (width, height), color = (73, 109, 137))
+ 
+	fnt = ImageFont.truetype('arial.ttf', 30)
+	d = ImageDraw.Draw(img)
+	d.text((10,10), text, font=fnt, fill=(255, 255, 0))
+	 
+	img.save(fileName)
 
 def hash_image(image_path):
     img = Image.open(image_path).resize((8,8), Image.LANCZOS).convert(mode="L")
@@ -14,7 +28,7 @@ def hash_image(image_path):
     return sum((1 if p > mean else 0) << i for i, p in enumerate(img.getdata()))
 
 def download_song(song):
-	fileName = ''.join([str(random.randint(1,9)) for i in range(10)]) + ".mp4"
+	fileName = "download_" + ''.join([str(random.randint(1,9)) for i in range(10)]) + ".mp4"
 	command = "youtube-dl -f 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4' "
 	command += '"ytsearch1:{} lyrics" '.format(song)
 	command += '--output "{}"'.format(fileName)
@@ -24,11 +38,9 @@ def download_song(song):
 def create_video():
     os.system("ffmpeg -r 1 -i frame%01d.png -vcodec mpeg4 -y movie.mp4")
 
-
-
-
-if __name__ == '__main__':
-	a = download_song('nine in the afternoon')
+def create_lyric_video(songName):
+	a = download_song(songName)
+	finalFileName = songName.replace(" ", "_") + ".mp4"
 	vidcap = cv2.VideoCapture(a)
 	success,image = vidcap.read()
 	count = 0
@@ -42,7 +54,7 @@ if __name__ == '__main__':
 		  count += 1
 	os.system("ffmpeg -i {} -f mp3 -ab 192000 -vn audio.mp3".format(a))
 	audio = MP4(a)
-	os.system("cp frame* test/")
+	# os.system("cp frame* test/")
 	# raw_input(audio.info.length)
 	# print(os.path.getsize(a))
 	fps = int(float(count) / audio.info.length)
@@ -64,10 +76,12 @@ if __name__ == '__main__':
 			vals.append(result)
 			valsInfo.append(imageVal)
 		# print(i)
+	for val in valsInfo:
+		if val != None:
+			recreate_image(val)
 	files = []
 	os.system("mkdir temp")
 	os.system("rm temp/*")
-	raw_input("Press to continue")
 	for k, v in db.iteritems():
 		command = "cp {} temp/{}".format(v, k)
 		os.system(command)
@@ -75,9 +89,21 @@ if __name__ == '__main__':
 	# os.system("mkdir temp")
 	os.system("ffmpeg -framerate 30 -i temp/frame%05d.jpg Project.mp4")
 	os.system("ffmpeg -i Project.mp4 -i audio.mp3 -c copy -map 0:v:0 -map 1:a:0 final.mp4")
+	os.system("rm temp/*")
+	os.system("rm Project.mp4")
+	os.system("rm *.mp3")
+	os.system("rm frame*")
+	os.system("rm download_*")
+	os.system("mv final.mp4 {}".format(finalFileName))
 	# allFiles = list(glob.glob("frame*.jpg"))
 	# allFiles.sort(key=lambda k: int(k.replace("frame", "").replace(".jpg", "")))
 
+
+
+if __name__ == '__main__':
+	songName = raw_input("Song Name: ")
+	create_lyric_video(songName)
+	
 	
 
 
